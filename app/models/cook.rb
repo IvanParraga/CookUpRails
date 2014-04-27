ad48587f33f8cded2c87c84122c21d66f2250c9f
Iván Párraga García <ivan.parraga.garcia@gmail.com>
2014-04-27 19:17:17 +0200

    adding validations

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,3 +1,6 @@
 class Cook < ActiveRecord::Base
   has_many :meals
+  validates_associated :meals
+  
+  validates :name, presence: true
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,6 @@
 class Meal < ActiveRecord::Base
   belongs_to :cook
+  
+  validates :name, :description, :price, :available_time, presence: true
+  validates :price, numericality: { only_integer: true }
 end
