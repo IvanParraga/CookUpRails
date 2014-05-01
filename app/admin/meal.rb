329a31ada9900d1d684e6c4334a678ac3c3edf86
Iván Párraga García <ivan.parraga.garcia@gmail.com>
2014-05-01 19:35:32 +0200

    add comments to meal admin to show picture

--- app/admin/meal.rb
+++ app/admin/meal.rb
@@ -1,17 +1,10 @@
 ActiveAdmin.register Meal do
-
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
   permit_params :name, :description, :price, :available_time, :cook_id, :order_id, :picture
+
+  # form do |f|
+  #   f.inputs "Details" do
+  #     f.input :picture, :as => :file, :hint => f.template.image_tag(f.object.picture.url)
+  #   end
+  #   f.actions
+  # end
 end
