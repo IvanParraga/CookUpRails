8c5e5337076537c0ba5cf49110b0dabd394c46d6
Iván Párraga García <ivan.parraga.garcia@gmail.com>
2014-05-01 20:43:34 +0200

    meal edit shows picture

--- app/admin/meal.rb
+++ app/admin/meal.rb
@@ -1,10 +1,16 @@
 ActiveAdmin.register Meal do
   permit_params :name, :description, :price, :available_time, :cook_id, :order_id, :picture
 
-  # form do |f|
-  #   f.inputs "Details" do
-  #     f.input :picture, :as => :file, :hint => f.template.image_tag(f.object.picture.url)
-  #   end
-  #   f.actions
-  # end
+  form do |f|
+    f.inputs do
+      f.input :cook
+      f.input :order
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :available_time
+      f.input :picture, :as => :file, :hint => f.template.image_tag(f.object.picture.url)
+    end
+    f.actions
+  end
 end
